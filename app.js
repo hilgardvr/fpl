072d786caca12b93ae6dc6461f7b46341d5c3b38
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-07-11 12:08:55 +0200

    update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const https = require('https');
 const path = require('path');
 const fplApi = 'https://fantasy.premierleague.com/api/bootstrap-static/';
